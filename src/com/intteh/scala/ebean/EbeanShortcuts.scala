b06d3b2da7785be610b8d8f8b2d0f03288ae60ab
Taras <tb@intteh.com>
2015-09-04 11:21:30 +0300

    initial version

---
+++ src/com/intteh/scala/ebean/EbeanShortcuts.scala
@@ -0,0 +1,285 @@
+package com.intteh.scala.ebean
+
+import scala.reflect.macros._
+import scala.language.experimental.macros
+import com.avaje.ebean._
+
+object EbeanShortcuts {
+
+  // not macro but has be be here as it result in ambiguous reference compiler error when placed into other object
+  def query(sql: String): SqlQuery = Ebean.createSqlQuery(sql)
+
+
+  def query[T <: Model](m: T, q: Boolean, select: Any*): Query[T] = macro queryMacroImpl[T]
+
+  def query[T <: Model](mClass: Class[T], m: T, q: Boolean, select: Any*): Query[T] = macro queryWClassMacroImpl[T]
+
+  def query[T <: Model](m: T, q: Boolean): Query[T] = macro queryWoSelectMacroImpl[T]
+
+  def query[T <: Model](mClass: Class[T], m: T, q: Boolean): Query[T] = macro queryWClassWoSelectMacroImpl[T]
+
+  def query[T <: Model](m: T): Query[T] = macro queryAllMacroImpl[T]
+
+  def expr(m: Model, q: Boolean): Expression = macro exprMacroImpl
+
+  def props(m: Model, select: Any*): String = macro propsMacroImpl
+
+  def in(m:Any,cond: Any*):Boolean = ??? //for macro only, never actually called
+  def like(m:Any, template: String):Boolean = ??? //never actually called
+  def raw(raw: String):Boolean = ??? //never actually called
+  def wrap(m: Expression):Boolean = ???
+
+
+  private abstract class CommonMacroCode {
+    val cw: blackbox.Context
+    import cw.universe._
+    val modelName: String
+
+
+
+    private val allFieldsMarker = "$"
+    private val modelPropRE = s"\\Q$modelName.\\E([a-zA-Z0-9_\\.]*)".r
+    private val modelPropWithImplicitRE = s"\\Qmacros.ebean.EbeanImplicits.\\E[a-zA-Z0-9_]*\\Q($modelName.\\E([a-zA-Z0-9_\\.]*)\\Q)\\E".r
+
+    private val modelNameWDot = modelName + "."
+    private val lengthOfModelNameWDot = modelNameWDot.length
+    private val e = q"com.avaje.ebean.Expr"
+
+    protected object N extends Enumeration {
+      type N = Value
+      val SubTree, Property, Val, ValMany = Value
+    }
+
+
+
+    def exception(msg: String) = cw.abort(cw.enclosingPosition, "EbeanShortcuts macro error. " + msg)
+
+    def processSelect(t: cw.Expr[Any]) = {
+      val s = t.tree.toString
+      if (s.startsWith(modelNameWDot)){
+        val r = s.substring(lengthOfModelNameWDot)
+        if(r.startsWith("`") && r.endsWith("`")) r.substring(1,r.length-1) else r
+      }
+      else exception(s"Select list must start with model name but is $s instead")
+    }
+
+    def makeQry(t: Tree, n: N.N = N.SubTree): Tree = {
+      n match {
+        case N.SubTree => {
+          val s = t.symbol
+          s match {
+            case sm if sm.isMethod => {
+              case class M(em: Tree, p1: N.N, p2: N.N, fnParamPos:Boolean = false)
+              val m = sm.asMethod
+
+              val em = m.name.toString match {
+                case "$greater" => new M(q"$e.gt", N.Property, N.Val)
+                case "$eq$eq" => new M(q"$e.eq", N.Property, N.Val)
+                case "$bang$eq" => new M(q"$e.ne", N.Property, N.Val)
+                case "$less" => new M(q"$e.lt", N.Property, N.Val)
+                case "$less$eq" => new M(q"$e.le", N.Property, N.Val)
+                case "$greater$eq" => new M(q"$e.ge", N.Property, N.Val)
+                case "$bar$bar" => new M(q"$e.or", N.SubTree, N.SubTree)
+                case "$amp$amp" => new M(q"$e.and", N.SubTree, N.SubTree)
+                case "unary_$bang" => new M(q"$e.not", N.SubTree, null)
+                case "in" =>   new M(q"$e.in",   N.Property, N.ValMany, true)
+                case "like" => new M(q"$e.like", N.Property, N.Val,true)
+                case "raw" =>  new M(q"$e.raw",  N.Val, null,true)
+                case "wrap" => new M(null,N.Val,null,true)
+                case "Boolean2boolean" =>  new  M(null,N.SubTree,null,true)
+                case emt@_ => exception(s"$emt method not handled")
+              }
+
+              val c = t.children
+              val opParamPosition = !em.fnParamPos
+
+              Option(em.p2) match {
+                case Some(p2m) => {
+                  val p1 = makeQry(if (opParamPosition) c.head.children.head else c.tail.head, em.p1)
+                  if(p2m!=N.ValMany) {
+                    val p2 = makeQry(if (opParamPosition) c.tail.head else c.tail.tail.head, p2m)
+                    q"${em.em}($p1,$p2)"
+                  } else {
+                    val coll = c.tail.tail.map(e=>makeQry(e,N.Val)).map(e=>Apply(Select(Ident(TermName("l")), TermName("add")),List(e)))
+                    val adds = Block(coll,Ident(TermName("l")))
+
+                    q"${em.em}($p1, {val l = new java.util.ArrayList[Object];$adds})"
+                  }
+                }
+                case None => {
+                  val p1 = makeQry(if(opParamPosition) c.head else c.tail.head, em.p1)
+                  Option(em.em) match {
+                    case Some(emm) => q"${em.em}($p1)"
+                    case None => p1
+                  }
+
+                }
+              }
+
+            }
+            case _ => {
+              // try parse as boolean property
+              t.toString match {
+                case modelPropRE(p) => q"$e.eq($p,true)"
+                case r@_ => exception(s"Unexpected, not method in ${t.toString}; Tried parse as property: Invalid property $r, expected $modelName.somepropname.")
+              }
+            }
+          }
+        }
+        case N.Property => {
+          t.toString match {
+            case modelPropRE(p) => q"$p"
+            case modelPropWithImplicitRE(p) => q"$p"
+            case r@_ => exception(s"Invalid property $r, expected $modelName.somepropname.")
+          }
+        }
+        case N.Val => t
+      }
+    }
+
+    def makeSelectAndFetch(selectAsStrings: Seq[String]):(Tree,Tree) = {
+      def makeFieldsList(l: Seq[String]) = if (l.contains(allFieldsMarker)) "*" else l.mkString(",")
+
+      val (fetchList, selectOnlyNoFetches) = selectAsStrings.partition(s => s.contains('.'))
+
+      val fetchList2 = fetchList.map(s => {
+        val p = s.lastIndexOf('.')
+        (s.substring(0, p), s.substring(p + 1))
+      })
+
+      val fetchList3: Map[String, Seq[(String, String)]] = fetchList2.groupBy(_._1)
+
+      val fetchList4 = fetchList3.map { case (k, v) => (k, v.map(_._2)) }
+
+      val fetchList5 = fetchList4.map { case (k, v) => (k, makeFieldsList(v)) }
+
+      val fetchListFinal = fetchList5.map { case (k, v) => (k, v) }(collection.breakOut)
+
+
+      val sl = if (selectOnlyNoFetches.isEmpty) "*" else makeFieldsList(selectOnlyNoFetches)
+      val sq = q"$sl"
+
+      def processFetch(l: Seq[(String, String)], a: Tree): Tree = {
+        if (l.isEmpty) a
+        else {
+          val h = l.head
+          val t = l.tail
+          Apply(Select(processFetch(t, a), TermName("fetch")), List(Literal(Constant(h._1)), Literal(Constant(h._2))))
+        }
+      }
+
+      val fetch = processFetch(fetchListFinal, Ident(TermName("q")))
+
+      (sq,fetch)
+    }
+  }
+
+  def propsMacroImpl(c: blackbox.Context)(m: c.Expr[Model], select: c.Expr[Any]*): c.Expr[String] = {
+    val w = new {
+      val cw: c.type = c
+      val modelName = m.tree.toString
+    } with CommonMacroCode
+
+    val selectAsStrings = select.map(w.processSelect)
+    val res = selectAsStrings.mkString(",")
+
+    import c.universe._
+    c.Expr[String](
+      q"$res"
+    )
+  }
+
+  def exprMacroImpl(c: blackbox.Context)(m: c.Expr[Model], q: c.Expr[Boolean]): c.Expr[com.avaje.ebean.Expression] = {
+    val w = new {
+      val cw: c.type = c
+      val modelName = m.tree.toString
+    } with CommonMacroCode
+
+    val qryResTree = w.makeQry(q.tree)
+
+    import c.universe._
+    c.Expr[com.avaje.ebean.Expression](
+      qryResTree
+    )
+  }
+
+  def queryAllMacroImpl[T <: Model : c.WeakTypeTag](c: blackbox.Context)(m: c.Expr[T]): c.Expr[Query[T]] = {
+    val w = new {
+      val cw: c.type = c
+      val modelName = m.tree.toString
+    } with CommonMacroCode
+    val modelType = c.weakTypeOf[T]
+    import c.universe._
+    c.Expr[com.avaje.ebean.Query[T]](
+      q"com.avaje.ebean.Ebean.createQuery(classOf[$modelType])"
+    )
+  }
+
+  def queryWClassWoSelectMacroImpl[T <: Model : c.WeakTypeTag](c: blackbox.Context)(mClass: c.Expr[Class[T]], m: c.Expr[T], q: c.Expr[Boolean]): c.Expr[Query[T]] = {
+    val w = new {
+      val cw: c.type = c
+      val modelName = m.tree.toString
+    } with CommonMacroCode
+
+    val qryResTree = w.makeQry(q.tree)
+
+    import c.universe._
+    c.Expr[com.avaje.ebean.Query[T]](
+      q"com.avaje.ebean.Ebean.createQuery($mClass).where($qryResTree)"
+    )
+  }
+
+  def queryWoSelectMacroImpl[T <: Model : c.WeakTypeTag](c: blackbox.Context)(m: c.Expr[T], q: c.Expr[Boolean]): c.Expr[Query[T]] = {
+    val w = new {
+      val cw: c.type = c
+      val modelName = m.tree.toString
+    } with CommonMacroCode
+
+    val qryResTree = w.makeQry(q.tree)
+
+    val modelType = c.weakTypeOf[T]
+    import c.universe._
+    c.Expr[com.avaje.ebean.Query[T]](
+      q"com.avaje.ebean.Ebean.createQuery(classOf[$modelType]).where($qryResTree)"
+    )
+  }
+
+  def queryMacroImpl[T <: Model : c.WeakTypeTag](c: blackbox.Context)(m: c.Expr[T], q: c.Expr[Boolean], select: c.Expr[Any]*): c.Expr[Query[T]] = {
+
+    val w = new {
+      val cw: c.type = c
+      val modelName = m.tree.toString
+    } with CommonMacroCode
+
+    val qryResTree = w.makeQry(q.tree)
+
+    val selectAsStrings = select.map(w.processSelect)
+    val (sq,fetch) = w.makeSelectAndFetch(selectAsStrings)
+
+    val modelType = c.weakTypeOf[T]
+    import c.universe._
+    c.Expr[com.avaje.ebean.Query[T]](
+      q"{val q=com.avaje.ebean.Ebean.createQuery(classOf[$modelType]).select($sq);val q2=$fetch;q2.where($qryResTree)}"
+    )
+  }
+
+  def queryWClassMacroImpl[T <: Model : c.WeakTypeTag](c: blackbox.Context)(mClass: c.Expr[Class[T]], m: c.Expr[T], q: c.Expr[Boolean], select: c.Expr[Any]*): c.Expr[Query[T]] = {
+
+    val w = new {
+      val cw: c.type = c
+      val modelName = m.tree.toString
+    } with CommonMacroCode
+
+    val qryResTree = w.makeQry(q.tree)
+
+    val selectAsStrings = select.map(w.processSelect)
+    val (sq,fetch) = w.makeSelectAndFetch(selectAsStrings)
+
+    import c.universe._
+    c.Expr[com.avaje.ebean.Query[T]](
+      q"{val q=com.avaje.ebean.Ebean.createQuery($mClass).select($sq);val q2=$fetch;q2.where($qryResTree)}"
+    )
+  }
+
+
+}
---
+++ src/com/intteh/scala/ebean/EbeanShortcutsNonMacro.scala
@@ -0,0 +1,71 @@
+package com.intteh.scala.ebean
+
+import com.avaje.ebean._
+
+object EbeanShortcutsNonMacro {
+
+  val sortDesc = " desc"
+  val sortAsc = " asc"
+
+  class EbeanTransactionControl(val server:EbeanServer, val transaction:Transaction){
+    def this() = this(Ebean.getServer(null),null)
+  }
+
+  def transaction[A](b:EbeanTransactionControl=>A):A = {
+    val server: EbeanServer = Ebean.getServer(null)
+    val tr: Transaction = server.createTransaction()
+    val dbTrC = new EbeanTransactionControl(server,tr)
+    try {
+      val a = b(dbTrC)
+      tr.commit()
+      tr.end()
+      a
+    } catch {
+      case e: Throwable =>
+        tr.rollback(e)
+        tr.end()
+        throw e
+    }
+  }
+
+  abstract class StoredProcedureParameter
+  case class In( value: Any) extends StoredProcedureParameter
+  case class Out( sqlType: Int) extends StoredProcedureParameter
+
+  def executeStoredProcedureMultiOut[T](name: String, params: StoredProcedureParameter*)(
+    implicit db: EbeanTransactionControl = new EbeanTransactionControl): Seq[T] ={
+    val paramsPlaceholders:String = params.map(_=>"?").mkString(",")
+    val cs = db.server.createCallableSql(s"{call $name($paramsPlaceholders)}")
+    val paramsWithIndex = params.zipWithIndex
+    for((p,i)<-paramsWithIndex){
+      p match {
+        case in: In => cs.setParameter(i+1,in.value)
+        case out: Out => cs.registerOut(i+1,out.sqlType)
+      }
+    }
+    db.server.execute(cs, db.transaction)
+
+    paramsWithIndex.filter(_._1.isInstanceOf[Out]).map{
+      case (p:Out,i) => cs.getObject(i+1).asInstanceOf[T]
+    }
+  }
+
+  def executeStoredProcedureReadOut[T](name: String,  outParam: Out, params: String*)(
+    implicit db: EbeanTransactionControl = new EbeanTransactionControl): T =
+    executeStoredProcedureMultiOut[T](
+      name,
+      params.map(p=> In(p)).toList ::: List(outParam): _*)(
+        db).head
+
+  def executeStoredProcedure(name: String, params: String*)(
+    implicit db: EbeanTransactionControl):Unit =
+    executeStoredProcedureMultiOut[AnyRef](name, params.map(p=> In(p)): _*)
+
+  def executeSql(sql: String)(implicit db: EbeanTransactionControl = new EbeanTransactionControl): Int = db.server.execute(db.server.createSqlUpdate(sql),db.transaction)
+
+  def save(m: Model)(implicit db: EbeanTransactionControl = new EbeanTransactionControl): Unit = db.server.save(m,db.transaction)
+
+  def delete(m: Model)(implicit db: EbeanTransactionControl = new EbeanTransactionControl): Unit = db.server.delete(m,db.transaction)
+
+
+}
